7823e185cf01d3783b481eedfeea3095db767242
Sannidhi Anvekar <anvekarsannidhi@gmail.com>
2026-06-08 00:24:16 +0530

    Connect PostgreSQL database

--- eventora/settings.py
+++ eventora/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'eventora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eventora_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Geeta@0202',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
