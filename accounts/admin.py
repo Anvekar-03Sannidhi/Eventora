c419ff609acfd9c574f0e4ac3990070f8dca4bf0
Sannidhi Anvekar <anvekarsannidhi@gmail.com>
2026-06-20 02:58:55 +0530

    Day1: Authentication system, role-based profiles and dashboard setup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "role", "phone_number")
+    list_filter = ("role",)
+    search_fields = ("user__username",)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def signup_view(request):
+
+    if request.method == "POST":
+
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            Profile.objects.create(
+                user=user,
+                role=form.cleaned_data["role"]
+            )
+
+            return redirect("signup")
+
+    else:
+
+        form = SignupForm()
+
+    return render(
+        request,
+        "accounts/signup.html",
+        {"form": form}
+    )
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            user = form.get_user()
+
+            login(request, user)
+
+            return redirect("dashboard")
+
+    else:
+
+        form = AuthenticationForm()
+
+    return render(
+        request,
+        "accounts/login.html",
+        {"form": form}
+    )
+
+def logout_view(request):
+
+    logout(request)
+
+    return redirect("/")
+
+@login_required
+def dashboard_view(request):
+
+    return render(
+        request,
+        "accounts/dashboard.html"
+    )
